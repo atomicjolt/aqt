3f11f919e8de99fe5e5bd77b274ff33710ac2dd4
Ryker Blunck <rykerblunck@gmail.com>
2021-01-13 11:05:02 -0700

    adding coffee-loader in attempt to get backbone extensions working

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -12,12 +12,18 @@ const handlebarsLoader = {
   loader: "handlebars-loader"
 }
 
+const coffeeLoader = {
+  test: /\.coffee$/,
+  loader: 'coffee-loader'
+}
+
 // Insert json loader at the end of list
 environment.loaders.append('graphql', graphqlLoader);
 environment.loaders.append('handlebars', handlebarsLoader);
+environment.loaders.append('coffeescript', coffeeLoader);
 
 environment.config.merge({resolve: {alias: {
-  Backbone: 'backbone',
+  Backbone: path.resolve(__dirname, '../../public/javascripts/Backbone.js'),
   jst: path.resolve(__dirname, '../../app/views/jst')
 }}})
 
